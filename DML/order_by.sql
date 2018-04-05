f483a7a763ebfc0e02dd60769d30a290aaaa66a1
takagotch <dyaccb@gmail.com>
2018-04-05 11:48:47 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ DML/order_by.sql
@@ -0,0 +1,14 @@
+SELECT * FROM foo ORDER BY a ASC
+SELECT * FROM foo ORDER BY a DESC
+
+//psgl
+SELECT * FROM foo ORDER BY a DESC NULLS LAST
+
+//
+SELECT a,b FROM foo ORDER BY 2
+
+SELECT a,c * b FROM foo ORDER BY 2
+
+
+
+
