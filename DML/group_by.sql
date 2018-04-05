d93d19dcaa75ea724474480226e1099f0b694dcb
takagotch <dyaccb@gmail.com>
2018-04-05 11:40:35 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- DML/group_by.sql
+++ DML/group_by.sql
@@ -1,9 +1,22 @@
-SELECT COUNT(*) FROM goo GROUP BY a
+SELECT 
+COUNT(*) 
+FROM goo 
+GROUP BY a
 
-SELECT name,COUNT(*) FROM results GROUP BY name
+
+SELECT name,
+COUNT(*) 
+FROM results 
+GROUP BY name
 
 //mysql
-SELECT a, SUM(b) FROM foo GROUP BY a DESC
+SELECT a,
+SUM(b) 
+FROM foo 
+GROUP BY a 
+DESC
+
+
 
 
 
