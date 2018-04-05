e83e4cadd9301e8daa4fc41201056e0aa0259650
takagotch <dyaccb@gmail.com>
2018-04-05 11:36:06 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ DML/WHERE.sql
@@ -0,0 +1,14 @@
+
+SELECT * FROM foo WHERE a = 1
+
+SELECT * FROM products WHERE price = 100
+
+SELECT * FROM foo,bar2 WHERE foo.a = bar2.a
+
+SELECT * FROM foo INNER JOIN bar ON foo.a = bar.a
+
+SELECT * FROM foo LEFT JOIN bar ON foo.a = bar.a
+
+
+
+
