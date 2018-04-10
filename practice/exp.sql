1c7d192ef194d249f94fbfe98950b56d6be395d4
takagotch <dyaccb@gmail.com>
2018-04-11 04:56:32 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ practice/exp.sql
@@ -0,0 +1,13 @@
+//
+
+exp system/manager full=y
+
+exp scott/tiger tables=(foo.bar)
+
+exp scott/tiger tables=(foo) file=foo.dmp
+
+exp scott/tiger@peta tables(foo)
+
+exp scott/tiger owner=(scott)
+
+
