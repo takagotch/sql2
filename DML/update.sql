8cd3fe94fd620100ec8d1a98257ab5f5a99c243c
takagotch <dyaccb@gmail.com>
2018-04-13 00:46:54 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- DML/update.sql
+++ DML/update.sql
@@ -1,13 +1,28 @@
 //UPDATE
 
-
+//UPDATE SET
 UPDATE foo SET a = 1
-
+//ROW UPDATE
 UPDATE foo SET a = 1 WHERE b = 2
-
+//MULTI COLUMN
 UPDATE foo SET a = 1,b = 2 WHERE c = 2
-
+//SUBQUERY UPDATE
 UPDATE foo SET a = (SELECT MAX(b) FROM bar) WHERE c = 1
+
+//FROM UPDATE
+UPDATE foo SET a = bar.c FROM bar
+  WHERE foo.b = bar.b
+
+//RETURNING,OUTPUT
+UPDATE foo SET a=2, b='two' WHERE a=1 RETURNING a, b
+UPDATE foo SET a=2, b='two' WHERE a=1 RETURNING a, b
+  INTO va, vb
+
+//TOP LIMIT
+UPDATE TOP(3) TOP SET a = 1
+UPDATE foo SET c = 1 LIMIT 3
+UPDATE foo sET c = 1 ORDER BY a LIMIT 3
+
 
 //psgl
 UPDATE foo SET a = bar.c FROM bar
@@ -19,6 +34,20 @@ UPDATE foo SET a=2, b='two' WHERE a=1 RETURNING a, b
 //mysql
 UPDATE TOP(3) foo SET a = 1
 UPDATE foo SET c = 1 LIMIT 3
-
 UPDATE foo SET c = 1 ORDER BY a LIMIT 3
+
+
+//sql
+DECLARE @table_var table(olda integer, newa integer,
+  b varchar(20))
+UPDATE foo SET a=2, b='two' OUTPUT DELETED.a,
+  INSERTED.a, DELETED.b INTO @table_var WHERE a=1
+----
+
+
+
+
+
+
+
 
