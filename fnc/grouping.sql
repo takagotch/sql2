ded340aded3abe5b97e11554dac4797c6cd87498
takagotch <dyaccb@gmail.com>
2018-04-14 19:02:33 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ fnc/grouping.sql
@@ -0,0 +1,12 @@
+//GROUPING
+//
+
+//oracle,db2
+SELECT sex,SUM(age),GROUPING(sex)
+  FROM addrlist GROUP BY ROLLUP(sex)
+
+//sql,db2
+SELECT sex,SUM(age),GROUPING(sex)
+  FROM addrlist GROUP BY sex WITH ROLLUP
+
+
