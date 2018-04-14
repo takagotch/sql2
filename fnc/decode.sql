c67b5f0b4743f5378f2013c95e26595fd212d82c
takagotch <dyaccb@gmail.com>
2018-04-15 04:29:31 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ fnc/decode.sql
@@ -0,0 +1,18 @@
+//DECODE
+//CASE
+//oracle
+
+SELECT a,DECODE(a,1,'success','error') FROM foo
+
+
+
+
+
+
+
+
+---
+
+
+
+
---
+++ fnc/isnull.sql
@@ -0,0 +1,13 @@
+//ISNULL
+//COALESCE
+//NVL
+//mysql
+
+SELECT a,ISNULL(a,-1) FROM foo
+SELECT b,ISNULL(b,'unknown') FROM foo
+
+---
+
+
+
+
